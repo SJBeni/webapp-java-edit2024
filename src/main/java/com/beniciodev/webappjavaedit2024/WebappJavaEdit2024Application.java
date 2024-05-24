9eebc6adf0f90b309171d09abac299d569e98640
sBenicioDev <sjbenicio@hotmail.com>
2024-05-24 14:30:34 -0300

    commit inicial

---
+++ src/main/java/com/beniciodev/webappjavaedit2024/WebappJavaEdit2024Application.java
@@ -0,0 +1,13 @@
+package com.beniciodev.webappjavaedit2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappJavaEdit2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappJavaEdit2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/beniciodev/webappjavaedit2024/WebappJavaEdit2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beniciodev.webappjavaedit2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappJavaEdit2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
